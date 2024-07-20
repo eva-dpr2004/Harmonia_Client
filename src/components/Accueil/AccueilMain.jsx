d66321af63d7969a570fe9d49265dac5a54f92a0
unknown <rosarioeva3@gmail.com>
2024-07-20 17:22:55 +0200

    AccueilStyleAndRefacto

--- src/components/Accueil/AccueilMain.jsx
+++ src/components/Accueil/AccueilMain.jsx
@@ -16,25 +16,25 @@ function AccueilMain() {
           Prenez soin de vos compagnons avec amour et simplicité, suivez leur santé et leur bien-être au bout des doigts !
         </h1>
         <div className="ImagesRow">
-          <div className="ImageContainer">
+          <div className="ImageContainer1">
             <img src={`${process.env.PUBLIC_URL}/assets/img/1.png`} alt="Img accueil 1" />
           </div>
-          <div className="ImageContainer">
+          <div className="ImageContainer2">
             <img src={`${process.env.PUBLIC_URL}/assets/img/2.png`} alt="Img accueil 2" />
           </div>
         </div>
         <p className="Description">
-          Avec notre calendrier personnalisés, ajoutez des rappels pour vous et vos animaux à l'aide de notifications !
+          Avec notre tableau d'activités, surveillez l'activité physique quotidienne et hebdomadaire de vos animaux !
         </p>
         <div className="ImagesRow">
-          <div className="ImageContainer">
+          <div className="ImageContainer3">
             <img src={`${process.env.PUBLIC_URL}/assets/img/3.png`} alt="Img accueil 3" />
           </div>
-          <div className="ImageContainer">
+          <div className="ImageContainer4">
             <img src={`${process.env.PUBLIC_URL}/assets/img/4.png`} alt="Img accueil 4" />
           </div>
         </div>
-        <p className="Description">
+        <p className="Description2">
           Surveillez leurs activités physiques dans un tableau répertoriant les sorties et découvrez la moyenne de temps pour chaque semaine !
         </p>
         <button className="StartButton" onClick={handleCommencerClick} style={{ cursor: 'pointer' }}>Commencer maintenant</button>
