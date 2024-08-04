851de24098b0a741ae9f5e0d933d174edc43693a
unknown <rosarioeva3@gmail.com>
2024-08-04 02:56:14 +0200

    Delete_Activities_Style_Fix

--- src/components/Activites/AjouterActivites.jsx
+++ src/components/Activites/AjouterActivites.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect, useContext, useCallback } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../../context/AuthContext';
-import { Select, MenuItem, TextField, Button, FormControl, InputLabel } from '@mui/material';
+import { Select, MenuItem, TextField, FormControl, InputLabel } from '@mui/material';
 import './Tableau.css';
 
 function AjouterActivites() {
-  const { authState } = useContext(AuthContext); 
+  const { authState } = useContext(AuthContext);
+
+  const getCurrentDate = () => {
+    const today = new Date();
+    return today.toISOString().split('T')[0];
+  };
+
   const [activity, setActivity] = useState({
     animalId: '',
-    date: '',
+    date: getCurrentDate(),
     debutActivite: '',
     finActivite: '',
     dureeActivite: ''
@@ -72,9 +78,16 @@ function AjouterActivites() {
     }
 
     const diffMs = finDate - debutDate;
-    const diffHrs = Math.floor(diffMs / (1000 * 60 * 60));
-    const diffMins = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
-    const dureeActivite = `${diffHrs}h ${diffMins}min`;
+    const diffMins = Math.floor(diffMs / (1000 * 60));
+    
+    if (diffMins < 1) {
+      setError('La durée de l\'activité doit être d\'au moins 1 minute.');
+      return;
+    }
+
+    const diffHrs = Math.floor(diffMins / 60);
+    const remainingMins = diffMins % 60;
+    const dureeActivite = `${diffHrs}h ${remainingMins}min`;
 
     const selectedAnimal = animaux.find(animal => animal.Id_Animal === parseInt(animalId));
     const animalNom = selectedAnimal ? selectedAnimal.Nom : '';
@@ -89,7 +102,7 @@ function AjouterActivites() {
         console.log('Activité ajoutée avec succès');
         setActivity({
           animalId: '',
-          date: '',
+          date: getCurrentDate(),
           debutActivite: '',
           finActivite: '',
           dureeActivite: ''
@@ -100,9 +113,19 @@ function AjouterActivites() {
         console.error('Erreur lors de l\'ajout de l\'activité');
       }
     } catch (error) {
-      console.error('Erreur lors de l\'ajout de l\'activité', error);
+      if (error.response && error.response.status === 400) {
+        setError(error.response.data.error);
+      } else {
+        console.error('Erreur lors de l\'ajout de l\'activité', error);
+      }
     }
   };
+
+  const animauxAvecActivites = [
+    'chat', 'chien', 'lapin', 'furet', 'singe', 'hérisson',
+    'perroquet', 'canari', 'poule', 'coq', 'canard', 'oie', 'dindon', 'perruche', 'pigeon', 'moineau',
+    'tortue', 'lézard', 'gecko', 'serpent', 'iguane', 'caméléon', 'grenouille', 'triton'
+  ];
 
   return (
     <div>
@@ -135,11 +158,13 @@ function AjouterActivites() {
                       <MenuItem value="">
                         <em>Aucun</em>
                       </MenuItem>
-                      {animaux.map(animal => (
-                        <MenuItem key={animal.Id_Animal} value={animal.Id_Animal}>
-                          {animal.Nom}
-                        </MenuItem>
-                      ))}
+                      {animaux
+                        .filter(animal => animauxAvecActivites.includes(animal.Espece.toLowerCase()))
+                        .map(animal => (
+                          <MenuItem key={animal.Id_Animal} value={animal.Id_Animal}>
+                            {animal.Nom}
+                          </MenuItem>
+                        ))}
                     </Select>
                   </FormControl>
                 </td>
@@ -153,6 +178,7 @@ function AjouterActivites() {
                     InputLabelProps={{
                       shrink: true,
                     }}
+                    disabled
                   />
                 </td>
                 <td>
@@ -182,9 +208,9 @@ function AjouterActivites() {
               </tr>
             </tbody>
           </table>
-          <Button type="submit" variant="contained" color="primary" className='AddActivité'>
-            Ajouter Activité
-          </Button>
+          <button type="submit" variant="contained" className='AddActivité'>
+            Ajouter l'Activité du jour
+          </button>
         </form>
       </div>
     </div>
--- src/components/Activites/TableauActivites.jsx
+++ src/components/Activites/TableauActivites.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect, useContext, useCallback } from 'react';
 import axios from 'axios';
 import { AuthContext } from '../../context/AuthContext';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
 import './Tableau.css';
 
 function TableauActivites() {
   const { authState } = useContext(AuthContext);
   const [activities, setActivities] = useState([]);
-  
+
   const fetchActivities = useCallback(() => {
     if (authState.isAuthenticated && authState.user?.Id_Utilisateur) {
       const url = `http://localhost:3001/activities/getActivitesByUserId/${authState.user.Id_Utilisateur}`;
@@ -24,33 +26,59 @@ function TableauActivites() {
     fetchActivities();
   }, [fetchActivities]);
 
+  const handleDelete = (id) => {
+    if (window.confirm('Suppression de l\'activité ? appuyer sur ok pour confirmer')) {
+      axios.delete(`http://localhost:3001/activities/deleteActivites/${id}`, { withCredentials: true })
+        .then((response) => {
+          console.log('Réponse de suppression:', response);
+          fetchActivities();
+        })
+        .catch(error => {
+          console.error('Erreur lors de la suppression de l\'activité:', error);
+        });
+    }
+  };
+
   return (
     <div>
-      <h2 className='title-activites'>Tableau des Activités</h2>
-      <div className='tableau'>
-        <table>
-          <thead>
-            <tr>
-              <th>Animal</th>
-              <th>Date</th>
-              <th>Début de l'activité</th>
-              <th>Fin de l'activité</th>
-              <th>Durée de l'activité</th>
-            </tr>
-          </thead>
-          <tbody>
-            {activities.map(activity => (
-              <tr key={activity.id}>
-                <td>{activity.Nom_Animal}</td>
-                <td>{activity.Date}</td>
-                <td>{activity.Debut_Activite}</td>
-                <td>{activity.Fin_Activite}</td>
-                <td>{activity.Duree_Activite}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {activities.length > 0 ? (
+        <div>
+          <h2 className='title-activites'>Tableau des Activités</h2>
+          <div className='tableau'>
+            <table>
+              <thead>
+                <tr>
+                  <th>Animal</th>
+                  <th>Date</th>
+                  <th>Début de l'activité</th>
+                  <th>Fin de l'activité</th>
+                  <th>Durée de l'activité</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {activities.map(activity => (
+                  <tr key={activity.Id_Activite}>
+                    <td>{activity.Nom_Animal}</td>
+                    <td>{activity.Date}</td>
+                    <td>{activity.Debut_Activite}</td>
+                    <td>{activity.Fin_Activite}</td>
+                    <td>{activity.Duree_Activite}</td>
+                    <td>
+                      <IconButton onClick={() => handleDelete(activity.Id_Activite)} aria-label="delete">
+                        <DeleteIcon />
+                      </IconButton>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <button>Calculer la moyenne de sortie</button>
+        </div>
+      ) : (
+        <p>Aucune activité enregistrée pour vos animaux.</p>
+      )}
     </div>
   );
 }
