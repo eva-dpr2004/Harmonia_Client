eb7cbbe975f4e5af0ab1ef94153e618792659680
unknown <rosarioeva3@gmail.com>
2024-08-05 04:47:00 +0200

    Duree_Quotidienne_Hebdomadaire_Activites

--- src/components/Activites/TableauActivites.jsx
+++ src/components/Activites/TableauActivites.jsx
@@ -3,11 +3,17 @@ import axios from 'axios';
 import { AuthContext } from '../../context/AuthContext';
 import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
+import { Select, MenuItem, FormControl, InputLabel, Card, CardContent, Typography } from '@mui/material';
 import './Tableau.css';
 
 function TableauActivites() {
   const { authState } = useContext(AuthContext);
   const [activities, setActivities] = useState([]);
+  const [selectedAnimal, setSelectedAnimal] = useState('');
+  const [showCard, setShowCard] = useState(false);
+  const [showWeeklyCard, setShowWeeklyCard] = useState(false);
+  const [totalTime, setTotalTime] = useState(0);
+  const [totalWeeklyTime, setTotalWeeklyTime] = useState(0);
 
   const fetchActivities = useCallback(() => {
     if (authState.isAuthenticated && authState.user?.Id_Utilisateur) {
@@ -39,12 +45,86 @@ function TableauActivites() {
     }
   };
 
+  const handleAnimalChange = (event) => {
+    setSelectedAnimal(event.target.value);
+    setShowCard(false);
+    setShowWeeklyCard(false);
+  };
+
+  const animalsWithActivities = [...new Set(activities.map(activity => activity.Nom_Animal))];
+
+  const filteredActivities = selectedAnimal
+    ? activities.filter(activity => activity.Nom_Animal === selectedAnimal)
+    : [];
+
+  const today = new Date().toISOString().split('T')[0];
+  const todaysActivities = filteredActivities.filter(activity => activity.Date === today);
+
+  const weekAgo = new Date();
+  weekAgo.setDate(weekAgo.getDate() - 7);
+  const lastWeekActivities = filteredActivities.filter(activity => new Date(activity.Date) > weekAgo);
+
+  const hasEnoughActivities = todaysActivities.length >= 2;
+
+  const convertMinutesToHoursAndMinutes = (minutes) => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${hours}h ${remainingMinutes}min`;
+  };
+
+  const calculateTotalTime = () => {
+    const totalMinutes = todaysActivities.reduce((total, activity) => {
+      const match = activity.Duree_Activite.match(/(\d+)h\s(\d+)min/);
+      const hours = match ? parseInt(match[1], 10) : 0;
+      const minutes = match ? parseInt(match[2], 10) : 0;
+      return total + (hours * 60) + minutes;
+    }, 0);
+    setTotalTime(totalMinutes);
+    setShowCard(true);
+  };
+
+  const calculateTotalWeeklyTime = () => {
+    const totalMinutes = lastWeekActivities.reduce((total, activity) => {
+      const match = activity.Duree_Activite.match(/(\d+)h\s(\d+)min/);
+      const hours = match ? parseInt(match[1], 10) : 0;
+      const minutes = match ? parseInt(match[2], 10) : 0;
+      return total + (hours * 60) + minutes;
+    }, 0);
+    setTotalWeeklyTime(totalMinutes);
+    setShowWeeklyCard(true);
+  };
+
+  const checkWeeklyActivities = () => {
+    const daysWithActivities = new Set(lastWeekActivities.map(activity => activity.Date));
+    return daysWithActivities.size >= 7;
+  };
+
   return (
     <div>
       {activities.length > 0 ? (
         <div>
           <h2 className='title-activites'>Tableau des Activités</h2>
-          <div className='tableau'>
+          <FormControl sx={{ width: '18%', marginBottom: '20px' }}>
+            <InputLabel id="animal-select-label">Sélectionner un animal</InputLabel>
+            <Select
+              labelId="animal-select-label"
+              value={selectedAnimal}
+              onChange={handleAnimalChange}
+              required
+              label="Sélectionner un animal"
+              sx={{ fontSize: '14px'}}
+            >
+              <MenuItem value="">
+                <em>Aucun</em>
+              </MenuItem >
+              {animalsWithActivities.map((animal, index) => (
+                <MenuItem key={index} value={animal} sx={{ fontSize: '14px'}}>
+                  {animal}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <div className='tableauActivites'>
             <table>
               <thead>
                 <tr>
@@ -57,24 +137,65 @@ function TableauActivites() {
                 </tr>
               </thead>
               <tbody>
-                {activities.map(activity => (
-                  <tr key={activity.Id_Activite}>
-                    <td>{activity.Nom_Animal}</td>
-                    <td>{activity.Date}</td>
-                    <td>{activity.Debut_Activite}</td>
-                    <td>{activity.Fin_Activite}</td>
-                    <td>{activity.Duree_Activite}</td>
-                    <td>
-                      <IconButton onClick={() => handleDelete(activity.Id_Activite)} aria-label="delete">
-                        <DeleteIcon />
-                      </IconButton>
-                    </td>
+                {filteredActivities.length > 0 ? (
+                  filteredActivities.map(activity => (
+                    <tr key={activity.Id_Activite}>
+                      <td>{activity.Nom_Animal}</td>
+                      <td>{activity.Date}</td>
+                      <td>{activity.Debut_Activite}</td>
+                      <td>{activity.Fin_Activite}</td>
+                      <td>{activity.Duree_Activite}</td>
+                      <td>
+                        <IconButton onClick={() => handleDelete(activity.Id_Activite)} aria-label="delete">
+                          <DeleteIcon sx={{ color: '#193356'}} />
+                        </IconButton>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan="6">Sélectionnez un animal pour voir ses activités.</td>
                   </tr>
-                ))}
+                )}
               </tbody>
             </table>
           </div>
-          <button>Calculer la moyenne de sortie</button>
+          <div className='BoutonsMoyenne'>
+            {hasEnoughActivities && (
+              <button variant="contained" className='AddActivité' onClick={calculateTotalTime}>
+                Calculer la durée totale quotidienne
+              </button>
+            )}
+            {checkWeeklyActivities() && (
+              <button variant="contained" className='AddActivité' onClick={calculateTotalWeeklyTime}>
+                Calculer la durée totale hebdomadaire
+              </button>
+            )}
+          </div>
+          {showCard && (
+            <Card className="animated-card">
+              <CardContent>
+                <Typography variant="h5" component="div" color="#183159">
+                  Total de temps d'activité pour aujourd'hui
+                </Typography>
+                <Typography variant="body2" color="#183159">
+                  {convertMinutesToHoursAndMinutes(totalTime)} aujourd'hui
+                </Typography>
+              </CardContent>
+            </Card>
+          )}
+          {showWeeklyCard && (
+            <Card className="animated-card">
+              <CardContent>
+                <Typography variant="h5" component="div" color="#183159">
+                  Total de temps d'activité pour la semaine
+                </Typography>
+                <Typography variant="body2" color="#183159">
+                  {convertMinutesToHoursAndMinutes(totalWeeklyTime)} cette semaine
+                </Typography>
+              </CardContent>
+            </Card>
+          )}
         </div>
       ) : (
         <p>Aucune activité enregistrée pour vos animaux.</p>
